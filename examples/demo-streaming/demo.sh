8c7e221591ab864f50e97220538410e9b782e1b6
marcoverl <marco.verlato@pd.infn.it>
2019-06-28 16:34:05 +0200

    Update demo.sh

--- examples/demo-streaming/demo.sh
+++ examples/demo-streaming/demo.sh
@@ -4,7 +4,7 @@ kubectl apply -f smactcluster.yaml
 # define the topics:
 kubectl apply -f topics.yaml
 #
-# find the endpoints to set in bootstrap.servers parameter and then exec the bin/kafka-run-class.sh inside kafka to populate# the kafka topic porduce28partitions:
+# find the endpoints to set in bootstrap.servers parameter and then exec the bin/kafka-run-class.sh inside kafka to populate the kafka topic produce28partitions:
 BSERV=$(kubectl describe service smactcluster-kafka-external-bootstrap | grep Endpoints | awk '{print $2}')
 kubectl exec -it smactcluster-kafka-0 -- bin/kafka-run-class.sh org.apache.kafka.tools.ProducerPerformance --topic produce28partitions --num-records 10000000000 --record-size 32 --throughput 100 --producer-props compression.type=snappy acks=1 bootstrap.servers=$BSERV buffer.memory=104857600 batch.size=50000
 #
